41c7320f538c3ebd749e01138f354157c2707ed6
Shraddha <shraddha200199@gmail.com>
2024-07-27 22:07:16 -0500

    Add flash maessages, favicon and link_to tag

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,8 @@
-class HomeController < ApplicationController
-  def index
+class AboutController < ApplicationController
+
+  def page
     flash[:notice] = "Login succesfully"
     flash[:alert] = "Invalid email or password"
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   root 'home#index'
+  get "about", to: "about#page"
 
 end
